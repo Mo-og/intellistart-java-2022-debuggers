86ae717356c7790606d8c1404f7e7eda132b6fe5
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-02 12:17:17 +0300

    :rocket: Create essential stubs(style)

--- src/main/java/com/intellias/intellistart/interviewplanning/models/InterviewerTimeSlot.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/models/InterviewerTimeSlot.java
@@ -1,22 +1,31 @@
 package com.intellias.intellistart.interviewplanning.models;
 
 import com.intellias.intellistart.interviewplanning.services.InterviewerSlotForm;
-import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Data;
 
+/**
+ * Interview Time slot.
+ */
 @Entity
 @Data
+
 public class InterviewerTimeSlot {
+
   @Id
   private Long id;
   private LocalTime from;
   private LocalTime to;
   private DayOfWeek dayOfWeek;
 
+  /**
+   * Constructor.
+   *
+   * @param form form
+   */
   public InterviewerTimeSlot(InterviewerSlotForm form) {
     from = form.getFrom();
     to = form.getTo();
--- src/test/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplicationTests.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplicationTests.java
@@ -1,11 +1,11 @@
 package com.intellias.intellistart.interviewplanning;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.intellias.intellistart.interviewplanning.services.InterviewerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class InterviewPlanningApplicationTests {
