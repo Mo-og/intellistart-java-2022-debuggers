b7f9765b9caa6bd6b80ce0e07b947354cf2343c8
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-06 12:15:19 +0300

    :label: implement Candidate

--- src/main/java/com/intellias/intellistart/interviewplanning/models/Candidate.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/models/Candidate.java
@@ -5,11 +5,19 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Candidate.
+ */
 public class Candidate extends User {
 
   private final Map<Long, CandidateTimeSlot> timeSlots;
   private final Set<Booking> bookings;
 
+  /**
+   * Constructor.
+   *
+   * @param email candidate email
+   */
   public Candidate(String email) {
     super(email, UserRole.CANDIDATE);
     timeSlots = new HashMap<>();
@@ -19,13 +27,14 @@ public class Candidate extends User {
   public void addSlot(CandidateTimeSlot slot) {
     //todo implements this
   }
-  public void addBooking(Booking booking){
+
+  public void addBooking(Booking booking) {
     bookings.add(booking);
     timeSlots.remove(booking.getId());
     //todo check this
   }
 
-  public void editSlot(long slotID) {
+  public void editSlot(long slotId) {
     //todo implements this
   }
 
