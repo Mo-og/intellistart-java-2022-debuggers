22591fb7b83054c7c1989660553f6f8eea37bcc2
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-06 11:25:35 +0300

    :label: implement Interviewer separated class

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/models/Interviewer.java
@@ -0,0 +1,35 @@
+package com.intellias.intellistart.interviewplanning.models;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Interviewer extends User {
+
+  private final Map<Long, InterviewerTimeSlot> timeSlots;
+  private final Set<Booking> bookings;
+
+  public Interviewer(String email) {
+    super(email, UserRole.INTERVIEWER);
+    timeSlots = new HashMap<>();
+    bookings = new HashSet<>();
+  }
+
+  public void addSlot(InterviewerTimeSlot slot) {
+    //todo implements this
+  }
+
+  public void addBooking(Booking booking) {
+    bookings.add(booking);
+    //todo check this
+  }
+
+  public void editSlot(long slotID) {
+    //todo implements this
+  }
+
+  public HashMap<Long, InterviewerTimeSlot> getSlots() {
+    return new HashMap<>(timeSlots);
+  }
+}
