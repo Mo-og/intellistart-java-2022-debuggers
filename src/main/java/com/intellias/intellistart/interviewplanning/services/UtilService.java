516bdb41e529466380c00f0081a31c2dd264c1e6
Olesia Yakovenko <olesjayakovenko@gmail.com>
2022-10-08 14:10:19 +0300

    :label: Define the number of week by date according to ISO 8601

--- src/main/java/com/intellias/intellistart/interviewplanning/services/UtilService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/UtilService.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
+import java.time.temporal.IsoFields;
+import java.time.temporal.WeekFields;
 
 /**
  * Provides week info.
@@ -14,15 +16,18 @@ public class UtilService {
   }
 
   public static int getCurrentWeekNum() {
+    // Todo Define the current week according to ISO 8601
     var date = ZonedDateTime.now(ZoneId.of("Europe/Kiev"));
     return date.get(ChronoField.ALIGNED_WEEK_OF_YEAR) + date.get(ChronoField.YEAR) * 100;
   }
 
   public static int getNextWeekNum() {
+    // Todo Define the next week according to ISO 8601
     return getCurrentWeekNum() + 1;
   }
 
   public static int getWeekNumByDate(LocalDate date) {
-    return date.get(ChronoField.ALIGNED_WEEK_OF_YEAR) + date.get(ChronoField.YEAR) * 100;
+    return date.get(WeekFields.ISO.weekOfWeekBasedYear())
+        + date.get(IsoFields.WEEK_BASED_YEAR) * 100;
   }
 }
