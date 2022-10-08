7bc1fbabfe3e7fb382b6d71a6c4d3bc1d74fb604
Olesia Yakovenko <olesjayakovenko@gmail.com>
2022-10-08 15:25:03 +0300

    :label: Add JavaDoc in UtilService

--- src/main/java/com/intellias/intellistart/interviewplanning/services/UtilService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/UtilService.java
@@ -15,17 +15,33 @@ public class UtilService {
   private UtilService() {
   }
 
+  /**
+   * Defines the number of the current week.
+   *
+   * @return number of week
+   */
   public static int getCurrentWeekNum() {
     // Todo Define the current week according to ISO 8601
     var date = ZonedDateTime.now(ZoneId.of("Europe/Kiev"));
     return date.get(ChronoField.ALIGNED_WEEK_OF_YEAR) + date.get(ChronoField.YEAR) * 100;
   }
 
+  /**
+   * Defines the number of the next week.
+   *
+   * @return number of week
+   */
   public static int getNextWeekNum() {
     // Todo Define the next week according to ISO 8601
     return getCurrentWeekNum() + 1;
   }
 
+  /**
+   * Defines the number of the week by date.
+   *
+   * @param date local date
+   * @return number of week
+   */
   public static int getWeekNumByDate(LocalDate date) {
     return date.get(WeekFields.ISO.weekOfWeekBasedYear())
         + date.get(IsoFields.WEEK_BASED_YEAR) * 100;
