309ee131fe4ca9b6ba2927bfe14c386b1d511822
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-14 00:32:40 +0300

    :rocket: implement booking controller

--- src/main/java/com/intellias/intellistart/interviewplanning/controllers/BookingController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/BookingController.java
@@ -1,14 +1,15 @@
 package com.intellias.intellistart.interviewplanning.controllers;
 
 import com.intellias.intellistart.interviewplanning.models.Booking;
+import com.intellias.intellistart.interviewplanning.models.Booking.BookingStatus;
+import com.intellias.intellistart.interviewplanning.models.CandidateTimeSlot;
+import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
 import com.intellias.intellistart.interviewplanning.services.BookingService;
-import com.intellias.intellistart.interviewplanning.services.CandidateService;
-import com.intellias.intellistart.interviewplanning.services.InterviewerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalTime;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,39 +19,58 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookingController {
 
   private final BookingService bookingService;
-  private final InterviewerService interviewerService;
-  private final CandidateService candidateService;
 
   /**
    * Constructor.
    *
-   * @param bookingService     booking service
-   * @param interviewerService interviewer service
-   * @param candidateService   candidate service
+   * @param bookingService booking service
    */
-  @Autowired
-  public BookingController(BookingService bookingService,
-      InterviewerService interviewerService,
-      CandidateService candidateService) {
+  public BookingController(BookingService bookingService) {
     this.bookingService = bookingService;
-    this.interviewerService = interviewerService;
-    this.candidateService = candidateService;
   }
 
+  /**
+   * Create booking for candidate and interviewer.
+   *
+   * @param interviewerTimeSlotId interviewer time slot id
+   * @param candidateTimeSlotId candidate time slot id
+   * @param from start time
+   * @param to end time
+   * @param subject subject of discussion
+   * @param description description of the booking
+   * @return booking //todo check this
+   */
   @PostMapping("/bookings")
-  public Booking createBooking(@RequestBody Booking booking) {
-    return bookingService.createBooking(booking);
+  public boolean createBooking(
+      @RequestParam("interviewerTimeSlotId") long interviewerTimeSlotId,
+      @RequestParam("candidateTimeSlotId") long candidateTimeSlotId,
+      @RequestParam("from") LocalTime from,
+      @RequestParam("to") LocalTime to,
+      @RequestParam("subject") String subject,
+      @RequestParam("description") String description) {
+
+    //todo find timeslots of candidate and interviewer for creating booking
+    CandidateTimeSlot candidateTimeSlot = new CandidateTimeSlot();
+    InterviewerTimeSlot interviewerTimeSlot = new InterviewerTimeSlot();
+
+
+    bookingService.createBooking(from, to, candidateTimeSlot, interviewerTimeSlot);
+    return true;
   }
 
   /**
    * Update booking.
    *
    * @param bookingId id of the booking
-   * @return booking
+   * @return booking  //todo real booking or true?
    */
   @PostMapping("/bookings/{bookingId}")
-  public Booking updateBooking(@PathVariable long bookingId, @RequestBody Booking booking) {
-    return bookingService.updateBooking(bookingId, booking);
+  public boolean updateBooking(@PathVariable long bookingId) {
+    Booking booking = bookingService.getBooking(bookingId);
+    booking.setFrom(LocalTime.now());
+    booking.setTo(LocalTime.now());
+    booking.setStatus(BookingStatus.BOOKED);
+    return true;
   }
 
   @DeleteMapping("/bookings/{bookingId}")
