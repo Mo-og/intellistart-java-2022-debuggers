5460a266462f032dae7ee6650c802a1795b62e60
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-14 00:32:08 +0300

    :label: add new method create booking

--- src/main/java/com/intellias/intellistart/interviewplanning/services/BookingService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/BookingService.java
@@ -1,9 +1,11 @@
 package com.intellias.intellistart.interviewplanning.services;
 
+import ch.qos.logback.core.util.Loader;
 import com.intellias.intellistart.interviewplanning.models.Booking;
 import com.intellias.intellistart.interviewplanning.models.CandidateTimeSlot;
 import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
 import com.intellias.intellistart.interviewplanning.repositories.BookingRepository;
+import java.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,9 +30,15 @@ public class BookingService {
    * @return booking
    */
   public Booking createBooking(CandidateTimeSlot candidateSlot,
-                               InterviewerTimeSlot interviewerSlot) {
+      InterviewerTimeSlot interviewerSlot) {
     //Todo calculate possible time
     return bookingRepository.save(new Booking(candidateSlot, interviewerSlot));
+  }
+
+  public Booking createBooking(LocalTime from, LocalTime to, CandidateTimeSlot candidateSlot,
+      InterviewerTimeSlot interviewerSlot) {
+    //Todo calculate possible time
+    return bookingRepository.save(new Booking(from, to, candidateSlot, interviewerSlot));
   }
 
 
