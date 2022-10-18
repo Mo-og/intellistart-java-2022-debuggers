8bf38cfc7f1bd5e676081e72fe87ccc77fdde849
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-19 00:54:46 +0300

    :boom: change links from Interviewer and Candidate to User

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/UserService.java
@@ -0,0 +1,68 @@
+package com.intellias.intellistart.interviewplanning.services;
+
+import com.intellias.intellistart.interviewplanning.exceptions.CoordinatorNotFoundException;
+import com.intellias.intellistart.interviewplanning.models.User;
+import com.intellias.intellistart.interviewplanning.models.User.UserRole;
+import com.intellias.intellistart.interviewplanning.repositories.UserRepository;
+import javax.persistence.EntityNotFoundException;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * User service.
+ */
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  /**
+   * Creates user and saves it to database.
+   *
+   * @param email user email
+   * @param role  user role according to which permissions will be granted
+   * @return user with generated id
+   */
+  public User create(String email, UserRole role) {
+    return userRepository.save(new User(email, role));
+  }
+
+  public User save(User user) {
+    return userRepository.save(user);
+  }
+
+
+  /**
+   * Returns a user of given id or generates an ApplicationErrorException if none found.
+   *
+   * @param id user id
+   * @return user with any role
+   */
+  public User getCoordinatorById(Long id) {
+    try {
+      return (User) Hibernate.unproxy(userRepository.getReferenceById(id));
+    } catch (EntityNotFoundException e) {
+      throw new CoordinatorNotFoundException(id);
+    }
+  }
+
+  /**
+   * Removes coordinator from database if id is valid or throws CoordinatorNotFoundException.
+   *
+   * @param id id to delete by
+   */
+  public void removeCoordinatorById(Long id) {
+    try {
+      userRepository.deleteById(id);
+    } catch (EntityNotFoundException e) {
+      throw new CoordinatorNotFoundException(id);
+    }
+  }
+
+}
