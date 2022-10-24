6e19f4b77b1c907fd578eefd5e7bd399eeb29769
mgorbik <mgorbiik@mail.ru>
2022-10-24 20:29:51 +0300

    New tests, code cleanup

--- src/main/java/com/intellias/intellistart/interviewplanning/models/User.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/models/User.java
@@ -49,10 +49,6 @@ public class User {
     this.userRole = userRole;
   }
 
-  public String getEmail() {
-    return email;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) {
