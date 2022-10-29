ed4fc33046b138f697e968b2a14126d5f95261a1
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-10-29 23:57:02 +0300

    :bulb: change paths in controller

--- src/main/java/com/intellias/intellistart/interviewplanning/controllers/BookingLimitController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/BookingLimitController.java
@@ -1,12 +1,13 @@
 package com.intellias.intellistart.interviewplanning.controllers;
 
 import com.intellias.intellistart.interviewplanning.models.BookingLimit;
-import com.intellias.intellistart.interviewplanning.models.dto.BookingLimitResponse;
+import com.intellias.intellistart.interviewplanning.models.dto.BookingLimitRequest;
 import com.intellias.intellistart.interviewplanning.services.BookingLimitService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,27 +23,20 @@ public class BookingLimitController {
   }
 
 
-  @PostMapping("/interviewers/current/bookingLimits")
-  public BookingLimit setBookingLimit(
-      @RequestParam Integer bookingLimit,
-      @RequestParam Long interviewerId,
-      @RequestParam Integer weekNum) {
-    return bookingLimitService.setBookingLimit(bookingLimit, interviewerId, weekNum);
+  @PostMapping("/interviewers/{interviewerId}/bookingLimits")
+  public BookingLimit setBookingLimit(@PathVariable Long interviewerId,
+      @RequestBody BookingLimitRequest request) {
+    return bookingLimitService.setBookingLimit(interviewerId, request);
   }
 
-  @GetMapping("/interviewers/bookingLimits/week")
-  public List<BookingLimit> getWeekBookingLimits(@RequestParam Integer weekNum) {
+  @GetMapping("/interviewers/bookingLimits/{weekNum}")
+  public List<BookingLimit> getWeekBookingLimits(@PathVariable Integer weekNum) {
     return bookingLimitService.getWeekBookingLimits(weekNum);
   }
 
-  @GetMapping("/interviewers/bookingLimits/week/user")
-  public BookingLimit getBookingLimit(@RequestParam Long interviewerId, Integer weekNum) {
+  @GetMapping("/interviewers/{interviewerId}/bookingLimits/{weekNum}")
+  public BookingLimit getBookingLimit(@PathVariable Long interviewerId,
+      @PathVariable Integer weekNum) {
     return bookingLimitService.getBookingLimit(interviewerId, weekNum);
-  }
-
-  @GetMapping("/interviewers/bookingLimits/week/user/limit")
-  public BookingLimitResponse getUserWeekBookingLimit(@RequestParam Long interviewerId,
-      Integer weekNum) {
-    return bookingLimitService.getUserWeekBookingLimit(interviewerId, weekNum);
   }
 }
