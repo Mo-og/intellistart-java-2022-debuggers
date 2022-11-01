42e6064681abffa768410335b5635852950aea72
Olesia Yakovenko <olesjayakovenko@gmail.com>
2022-11-01 03:04:30 +0200

    :heavy_plus_sign: Implement findByInterviewerSlot

--- src/main/java/com/intellias/intellistart/interviewplanning/repositories/BookingRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repositories/BookingRepository.java
@@ -1,6 +1,8 @@
 package com.intellias.intellistart.interviewplanning.repositories;
 
 import com.intellias.intellistart.interviewplanning.models.Booking;
+import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+  Set<Booking> findByInterviewerSlot(InterviewerTimeSlot interviewerSlot);
 }
