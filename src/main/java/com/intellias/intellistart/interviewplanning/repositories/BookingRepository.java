1edb1c8b2df52703b7f3c573ac3207c1ad79c161
Olesia Yakovenko <olesjayakovenko@gmail.com>
2022-11-01 10:58:10 +0200

    :heavy_plus_sign: Implement findByCandidateSlot and findByCandidateSlotDate

--- src/main/java/com/intellias/intellistart/interviewplanning/repositories/BookingRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repositories/BookingRepository.java
@@ -1,7 +1,9 @@
 package com.intellias.intellistart.interviewplanning.repositories;
 
 import com.intellias.intellistart.interviewplanning.models.Booking;
+import com.intellias.intellistart.interviewplanning.models.CandidateTimeSlot;
 import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
+import java.time.LocalDate;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,8 @@ import org.springframework.stereotype.Repository;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
   Set<Booking> findByInterviewerSlot(InterviewerTimeSlot interviewerSlot);
+
+  Set<Booking> findByCandidateSlot(CandidateTimeSlot candidateSlot);
+
+  Set<Booking> findByCandidateSlotDate(LocalDate date);
 }
