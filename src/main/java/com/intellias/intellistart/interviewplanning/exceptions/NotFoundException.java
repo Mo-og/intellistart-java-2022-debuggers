569bad395812b31823ffae18b321d9ccf4612f22
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-11-03 10:40:16 +0200

    add new class for all NOT FOUND exceptions

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/exceptions/NotFoundException.java
@@ -0,0 +1,48 @@
+package com.intellias.intellistart.interviewplanning.exceptions;
+
+public class NotFoundException extends ApplicationErrorException {
+
+  public NotFoundException(ErrorCode errorCode, String errorMessage) {
+    super(errorCode, errorMessage);
+  }
+
+  public static NotFoundException userNotFound(String email) {
+    return new NotFoundException(ErrorCode.USER_NOT_FOUND, " with email: " + email);
+  }
+
+  public static NotFoundException userNotFound() {
+    return new NotFoundException(ErrorCode.USER_NOT_FOUND, "");
+  }
+
+  public static NotFoundException candidateNotFound(Long id) {
+    return new NotFoundException(ErrorCode.CANDIDATE_NOT_FOUND, " with id: " + id);
+  }
+
+  public static NotFoundException candidateNotFound() {
+    return new NotFoundException(ErrorCode.CANDIDATE_NOT_FOUND, "");
+  }
+
+  public static NotFoundException interviewerNotFound(Long id) {
+    return new NotFoundException(ErrorCode.INTERVIEWER_NOT_FOUND, " with id: " + id);
+  }
+
+  public static NotFoundException interviewerNotFound() {
+    return new NotFoundException(ErrorCode.INTERVIEWER_NOT_FOUND, "");
+  }
+
+  public static NotFoundException coordinatorNotFound(Long id) {
+    return new NotFoundException(ErrorCode.COORDINATOR_NOT_FOUND, " with id: " + id);
+  }
+
+  public static NotFoundException coordinatorNotFound() {
+    return new NotFoundException(ErrorCode.COORDINATOR_NOT_FOUND, "");
+  }
+
+  public static NotFoundException timeSlotNotFound(Long id) {
+    return new NotFoundException(ErrorCode.COORDINATOR_NOT_FOUND, " with id: " + id);
+  }
+
+  public static NotFoundException timeSlotNotFound() {
+    return new NotFoundException(ErrorCode.COORDINATOR_NOT_FOUND, "");
+  }
+}
