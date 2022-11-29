a35abc36d757f5035a8c0785afa17624a8819868
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-11-29 12:57:52 +0200

    :sparkles: Specify error messages

--- src/main/java/com/intellias/intellistart/interviewplanning/validators/PeriodValidator.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/validators/PeriodValidator.java
@@ -1,6 +1,7 @@
 package com.intellias.intellistart.interviewplanning.validators;
 
 import com.intellias.intellistart.interviewplanning.exceptions.InvalidInputException;
+import java.time.Duration;
 import java.time.LocalTime;
 
 /**
@@ -8,6 +9,10 @@ import java.time.LocalTime;
  */
 public class PeriodValidator {
 
+  private static final LocalTime lowerTimeExclusive = LocalTime.of(7, 59);
+  private static final LocalTime upperTimeExclusive = LocalTime.of(22, 1);
+  private static final Integer roundToMinutes = 30;
+  private static final Integer minPeriodInMinutes = 90;
   /**
    * Checks if period is valid.
    *
@@ -15,14 +20,13 @@ public class PeriodValidator {
    * @param to   end time
    * @return true if period is valid
    */
-  public static boolean isValid(String from, String to) {
-    LocalTime fromTime = LocalTime.parse(from);
-    LocalTime toTime = LocalTime.parse(to);
-
-    return toTime.minusMinutes(89).isAfter(fromTime)
-        && Math.abs(toTime.getMinute() - fromTime.getMinute()) % 30 == 0
-        && fromTime.isAfter(LocalTime.of(7, 59))
-        && toTime.isBefore(LocalTime.of(22, 1));
+  public static boolean isValid(LocalTime from, LocalTime to) {
+    return to.minusMinutes(89).isAfter(from)
+        && to.getMinute() % roundToMinutes == 0
+        && from.getMinute() % roundToMinutes == 0
+        && from.isAfter(lowerTimeExclusive)
+        && to.isBefore(upperTimeExclusive)
+        && Duration.between(to, from).toMinutes() >= minPeriodInMinutes;
   }
 
   /**
@@ -32,9 +36,17 @@ public class PeriodValidator {
    * @param to   end time
    * @throws InvalidInputException if period is invalid
    */
-  public static void validate(String from, String to) {
-    if (!isValid(from, to)) {
-      throw InvalidInputException.boundaries(from, to);
+  public static void validate(LocalTime from, LocalTime to) {
+    if (to.getMinute() % roundToMinutes != 0) {
+      throw InvalidInputException.notRounded(to.toString());
+    } else if (from.getMinute() % roundToMinutes != 0) {
+      throw InvalidInputException.notRounded(from.toString());
+    } else if (!from.isAfter(lowerTimeExclusive)) {
+      throw InvalidInputException.timeLowerBound();
+    } else if (!to.isBefore(upperTimeExclusive)) {
+      throw InvalidInputException.timeUpperBound();
+    } else if (Duration.between(to, from).toMinutes() < minPeriodInMinutes) {
+      throw InvalidInputException.minPeriod();
     }
   }
 
