22b2454028cc1dd19e8059b7dc650ea02811233b
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-12-02 21:52:03 +0200

    Refactor period validation, Add period intersection validation

--- src/main/java/com/intellias/intellistart/interviewplanning/validators/PeriodValidator.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/validators/PeriodValidator.java
@@ -1,8 +1,11 @@
 package com.intellias.intellistart.interviewplanning.validators;
 
 import com.intellias.intellistart.interviewplanning.exceptions.InvalidInputException;
+import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.List;
 
 /**
  * PeriodValidator class. Uses to validate from/to time.
@@ -15,12 +18,9 @@ public class PeriodValidator {
   private static final Integer minPeriodInMinutes = 90;
 
   /**
-   * Checks if period is valid.
-   * from/to time should be rounded to 00 or 30 minutes
-   * from time should be after 7:59 AM
-   * to time should be before 22:01 PM
-   * minimum period should be 1.5 hours
-   * 
+   * Checks if period is valid. from/to time should be rounded to 00 or 30 minutes from time should
+   * be after 7:59 AM to time should be before 22:01 PM minimum period should be 1.5 hours
+   *
    * @param from start time
    * @param to   end time
    * @return true if period is valid
@@ -31,15 +31,12 @@ public class PeriodValidator {
         && from.getMinute() % roundToMinutes == 0
         && from.isAfter(lowerTimeExclusive)
         && to.isBefore(upperTimeExclusive)
-        && Duration.between(to, from).toMinutes() >= minPeriodInMinutes;
+        && Duration.between(from, to).toMinutes() >= minPeriodInMinutes;
   }
 
   /**
-   * Method to validate period.
-   * from/to time should be rounded to 00 or 30 minutes
-   * from time should be after 7:59 AM
-   * to time should be before 22:01 PM
-   * minimum period should be 1.5 hours
+   * Method to validate period. from/to time should be rounded to 00 or 30 minutes. from time should
+   * be after 7:59 AM. to time should be before 22:01 PM. minimum period should be 1.5 hours.
    *
    * @param from start time
    * @param to   end time
@@ -47,16 +44,34 @@ public class PeriodValidator {
    */
   public static void validate(LocalTime from, LocalTime to) {
     if (to.getMinute() % roundToMinutes != 0) {
-      throw InvalidInputException.notRounded(to.toString());
+      throw InvalidInputException.rounding(to.toString());
     } else if (from.getMinute() % roundToMinutes != 0) {
-      throw InvalidInputException.notRounded(from.toString());
+      throw InvalidInputException.rounding(from.toString());
     } else if (!from.isAfter(lowerTimeExclusive)) {
       throw InvalidInputException.timeLowerBound();
     } else if (!to.isBefore(upperTimeExclusive)) {
       throw InvalidInputException.timeUpperBound();
-    } else if (Duration.between(to, from).toMinutes() < minPeriodInMinutes) {
+    } else if (Duration.between(from, to).toMinutes() < minPeriodInMinutes) {
       throw InvalidInputException.minPeriod();
     }
+  }
+
+  /**
+   * Method to validate intersection with other slots.
+   *
+   * @param from     start time
+   * @param to       end time
+   * @param allSlots list of slots
+   */
+  public static void validateIntersection(LocalTime from, LocalTime to, DayOfWeek dayOfWeek,
+      List<InterviewerTimeSlot> allSlots) {
+    allSlots.stream()
+        .filter(slot -> slot.getDayOfWeek().equals(dayOfWeek))
+        .forEach((slot) -> {
+          if (!(slot.getFrom().isAfter(to) || slot.getTo().isBefore(from))) {
+            throw InvalidInputException.periodIntersection();
+          }
+        });
   }
 
   private PeriodValidator() {
--- src/test/java/com/intellias/intellistart/interviewplanning/validators/PeriodValidatorTests.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/validators/PeriodValidatorTests.java
@@ -6,34 +6,77 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.intellias.intellistart.interviewplanning.exceptions.InvalidInputException;
+import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
+import com.intellias.intellistart.interviewplanning.services.WeekServiceImp;
+import com.intellias.intellistart.interviewplanning.services.interfaces.WeekService;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class PeriodValidatorTests {
 
+  private static final WeekService weekService = new WeekServiceImp();
+
   @Test
   void testIsValidRight() {
-    assertTrue(PeriodValidator.isValid("08:00", "09:30"));
-    assertTrue(PeriodValidator.isValid("08:15", "09:45"));
-    assertTrue(PeriodValidator.isValid("08:00", "22:00"));
+    assertTrue(PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(9, 30)));
+    assertTrue(PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(10, 0)));
+    assertTrue(PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(22, 0)));
   }
 
   @Test
   void testIsValidWrong() {
-    assertFalse(PeriodValidator.isValid("08:00", "09:00"));
-    assertFalse(PeriodValidator.isValid("07:00", "09:00"));
-    assertFalse(PeriodValidator.isValid("08:00", "23:00"));
-    assertFalse(PeriodValidator.isValid("08:00", "10:10"));
+    assertFalse(PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(9, 0)));
+    assertFalse(PeriodValidator.isValid(LocalTime.of(7, 0), LocalTime.of(9, 30)));
+    assertFalse(PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(23, 0)));
+    assertFalse(PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(10, 10)));
+  }
+
+  @Test
+  void testValidateCorrectPeriod() {
+    assertDoesNotThrow(() -> PeriodValidator.isValid(LocalTime.of(8, 0), LocalTime.of(9, 30)));
+    assertDoesNotThrow(() -> PeriodValidator.validate(LocalTime.of(8, 0), LocalTime.of(10, 0)));
+    assertDoesNotThrow(() -> PeriodValidator.validate(LocalTime.of(8, 0), LocalTime.of(22, 0)));
   }
 
   @Test
   void testValidateWrongPeriod() {
     assertThrows(InvalidInputException.class,
-        () -> PeriodValidator.validate("08:00", "10:10"));
+        () -> PeriodValidator.validate(LocalTime.of(8, 0), LocalTime.of(9, 0)));
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validate(LocalTime.of(7, 0), LocalTime.of(9, 30)));
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validate(LocalTime.of(8, 0), LocalTime.of(23, 0)));
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validate(LocalTime.of(8, 0), LocalTime.of(10, 10)));
   }
 
   @Test
-  void testValidateCorrectPeriod() {
-    assertDoesNotThrow(() -> PeriodValidator.validate("08:00", "22:00"));
+  void testValidateIntersection() {
+    List<InterviewerTimeSlot> slots = List.of(
+        new InterviewerTimeSlot("08:00", "09:30", "Mon", weekService.getNextWeekNum()),
+        new InterviewerTimeSlot("10:30", "12:30", "Mon", weekService.getNextWeekNum()),
+        new InterviewerTimeSlot("12:30", "14:00", "Mon", weekService.getNextWeekNum()),
+        new InterviewerTimeSlot("14:30", "17:00", "Mon", weekService.getNextWeekNum())
+    );
+
+    assertDoesNotThrow(() ->
+        PeriodValidator.validateIntersection(
+            LocalTime.of(17, 30), LocalTime.of(19, 0), DayOfWeek.MONDAY, slots));
+
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validateIntersection(
+            LocalTime.of(8, 0), LocalTime.of(9, 30), DayOfWeek.MONDAY, slots));
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validateIntersection(
+            LocalTime.of(9, 0), LocalTime.of(10, 30), DayOfWeek.MONDAY, slots));
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validateIntersection(
+            LocalTime.of(17, 0), LocalTime.of(19, 30), DayOfWeek.MONDAY, slots));
+    assertThrows(InvalidInputException.class,
+        () -> PeriodValidator.validateIntersection(
+            LocalTime.of(8, 0), LocalTime.of(17, 0), DayOfWeek.MONDAY, slots));
   }
 
 }
